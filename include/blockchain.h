e505bc02445bbd6b05da0a8c7b3a3b0a65070f95
miglecin <migle.cincikaite@mif.stud.vu.lt>
2025-10-12 12:03:04 +0300

    feat: blockchain API header

---
+++ include/blockchain.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "models.h"
+#include <deque>
+#include <vector>
+#include <string>
+
+class Blockchain {
+public:
+    explicit Blockchain(std::string diff_prefix = "000");
+    void init_users(size_t n_users);
+    void init_transactions(size_t n_txs);
+    bool mine_next_block(size_t block_size);
+
+    const std::vector<Block>& chain() const { return chain_; }
+    const std::deque<Transaction>& mempool() const { return mempool_; }
+
+private:
+    std::string calc_tx_id(const Transaction& t) const;
+    std::string calc_txs_hash(const std::vector<Transaction>& txs) const;
+    std::string serialize_header(const BlockHeader& h) const;
+    std::string hash_header(const BlockHeader& h) const;
+    bool valid_pow(const std::string& hex) const;
+    void apply_block_effects(const Block& b);
+
+private:
+    //std::vector<User> users_;             
+    std::deque<Transaction> mempool_;
+    std::vector<Block> chain_;
+    std::string difficulty_;
+};
